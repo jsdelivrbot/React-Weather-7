996e1f525f6a5a0dde03068099ad0de8ad178c9a
danielnaxon-inmanage <danielnaxon@inmanage.net>
2017-02-25 01:09:03 +0200

    upload to git

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = '33062583185a804e9dad3a19a984bb12';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}&units=metric`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city}`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
